45845ccd43b7018406faecba515f5f6f39cc6e61
Samet-Furkan-Kopar <sametkopar465@gmail.com>
2024-05-06 15:56:22 +0300

    speech to text page added

--- src/app/(root)/(home)/speech-to-text/page.tsx
+++ src/app/(root)/(home)/speech-to-text/page.tsx
@@ -2,7 +2,7 @@
 import useSpeechToText from "@/components/useSpeechToText";
 import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { BsFiletypePdf } from "react-icons/bs";
 import { IoCopyOutline } from "react-icons/io5";
@@ -13,6 +13,9 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { jsPDF } from "jspdf";
+import { saveAs } from "file-saver";
+import { FaRegFileWord } from "react-icons/fa";
 
 const SpeechToText = () => {
     const [textInput, setTextInput] = useState("");
@@ -22,7 +25,7 @@ const SpeechToText = () => {
         continuous: true,
         lang: lang,
     });
-    
+
     const startStopListening = () => {
         isListening ? stopVoiceInput() : startListening();
     };
@@ -45,16 +48,30 @@ const SpeechToText = () => {
             toast({ title: "Text Copied." });
         }
     };
+    const downloadPDF = () => {
+        const pdf = new jsPDF();
+        pdf.addFont("fonts/Roboto-Medium.ttf", "TurkishFont", "normal");
+        pdf.setFont("TurkishFont");
+        pdf.text(textInput, 20, 25);
+        const pdfData = pdf.output();
+        const blob = new Blob([pdfData], { type: "application/pdf" });
+        const blobURL = URL.createObjectURL(blob);
+        window.open(blobURL);
+    };
+    const downloadWord = () => {
+        const blob = new Blob([textInput], { type: "application/msword" });
+        saveAs(blob, "speech_text.doc");
+    };
 
     return (
         <div className="flex items-center justify-center my-8">
-            <Card className="flex flex-col w-1/2 h-[500px] bg-slate-100 min-w-[250px]">
-                <CardHeader className="flex flex-row space-y-0 justify-between items-center">
-                    <CardTitle className="flex text-center">
+            <Card className="flex flex-col w-1/2 h-screen max-h-[600px] bg-slate-100 min-w-[250px] md:gap-6">
+                <CardHeader className="flex flex-row space-y-0 justify-between items-center gap-1" >
+                    <CardTitle className="flex text-center max-md:text-sm">
                         {!isListening ? "Mic is Off" : "Mic is On"}
                     </CardTitle>
-                    <div className="flex flex-row gap-1">
-                        <FormControl sx={{ minWidth: 120 }} size="small">
+                    <div className="flex items-center justify-between md:gap-5">
+                        <FormControl className="w-[120px] max-md:w-[80px] " size="small">
                             <InputLabel id="demo-select-small-label">Language</InputLabel>
                             <Select
                                 labelId="demo-select-small-label"
@@ -62,6 +79,7 @@ const SpeechToText = () => {
                                 label="Language"
                                 value={lang}
                                 onChange={handleChange}
+                                className="max-md:text-sm"
                             >
                                 <MenuItem value={lang}></MenuItem>
                                 <MenuItem value="tr-TR">Türkçe</MenuItem>
@@ -74,28 +92,57 @@ const SpeechToText = () => {
                                 <MenuItem value="fr-FR">Français</MenuItem>
                             </Select>
                         </FormControl>
+                       <div className="flex max-md:flex-col justify-between gap-0">
+                       <Tooltip title="Download as PDF">
+                            <Button
+                                variant="text"
+                                tabIndex={-1}
+                                sx={{ m:0 }}
+                                startIcon={<BsFiletypePdf className="size-[30px] max-md:size-[20px]" color="black" />}
+                                onClick={() => {
+                                    if (textInput) {
+                                        downloadPDF();
+                                    } else {
+                                        toast({ title: "No text yet" });
+                                    }
+                                }}
+                            />
+                        </Tooltip>
+                        <Tooltip title="Download as Word">
+                            <Button
+                                variant="text"
+                                tabIndex={-1}
+                                startIcon={<FaRegFileWord className="size-[30px] max-md:size-[20px]" color="black" />}
+                                onClick={() => {
+                                    if (textInput) {
+                                        downloadWord();
+                                    } else {
+                                        toast({ title: "No text yet" });
+                                    }
+                                }}
+                            />
+                        </Tooltip>
+                        <Tooltip title="Copy Text">
                         <Button
                             variant="text"
                             tabIndex={-1}
-                            startIcon={<BsFiletypePdf size={25} color="black" />}
-                        ></Button>
-                        <Button
-                            variant="text"
-                            tabIndex={-1}
-                            startIcon={<IoCopyOutline size={25} color="black" />}
+                            startIcon={<IoCopyOutline className="size-[30px] max-md:size-[20px]"  color="black" />}
                             onClick={copyText}
-                        ></Button>
+                        />
+                        </Tooltip>
+                       </div>
                     </div>
                 </CardHeader>
-                <CardContent className="relative">
+                <CardContent className="relative p-0 " >
                     <TextField
                         id="filled-multiline-static"
                         multiline
                         fullWidth
-                        rows={16}
+                        rows={18}
                         disabled={isListening}
+                        // className="mt-5"
                         sx={{
-                            backgroundColor: "#F1F5F9" ,
+                            backgroundColor: "#F1F5F9",
                             border: "none",
                         }}
                         value={
@@ -109,7 +156,7 @@ const SpeechToText = () => {
                         onChange={(e) => setTextInput(e.target.value)}
                     />
                     <Button
-                        className="absolute left-[50%] bottom-[20%]"
+                        className="absolute left-[calc(50%-35px)] bottom-[20%]"
                         variant="text"
                         tabIndex={-1}
                         startIcon={
